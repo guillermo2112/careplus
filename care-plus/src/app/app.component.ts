00a9b0c3a7103cd00e9ed9a8f9de96a884cf4de6
marianazanoletty-eviden <166487190+marianazanoletty-eviden@users.noreply.github.com>
2024-05-08 18:25:32 +0200

    carousel

--- care-plus/src/app/app.component.ts
+++ care-plus/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { CarouselComponent } from "./components/carousel/carousel.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent, FooterComponent]
+    imports: [RouterOutlet, NavbarComponent, FooterComponent, CarouselComponent]
 })
 export class AppComponent {
   title = 'care-plus';
