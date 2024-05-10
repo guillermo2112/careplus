d63c4aa0d9660cb7983e8d5b09f66e2708206521
guillermo2112 <guillermo.moreno.external@eviden.com>
2024-05-10 12:00:38 +0200

    routing

--- care-plus/src/app/app.routes.ts
+++ care-plus/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar/navbar.component';
 import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {path: 'navbar', component: NavbarComponent},
+    {path: 'home', component: HomeComponent},
     {path: 'quienes_somos', component: QuienesSomosComponent},
     {path: 'inicio_sesion', component: LoginComponent},
 ];
---
+++ care-plus/src/app/components/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CarouselComponent } from "../carousel/carousel.component";
+import { CardespecialidadesComponent } from "../cardespecialidades/cardespecialidades.component";
+
+@Component({
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [CarouselComponent, CardespecialidadesComponent]
+})
+export class HomeComponent {
+
+}
