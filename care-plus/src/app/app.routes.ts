dab2ad7f78460f641ee4e836ab1106208ca33c15
guillermo2112 <guillermo.moreno.external@eviden.com>
2024-05-10 11:49:05 +0200

    routing v4

--- care-plus/src/app/app.routes.ts
+++ care-plus/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar/navbar.component';
+import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-    {path: 'navbar', component: NavbarComponent}
+    {path: 'navbar', component: NavbarComponent},
+    {path: 'quienes_somos', component: QuienesSomosComponent},
+    {path: 'inicio_sesion', component: LoginComponent},
 ];
--- care-plus/src/app/components/navbar/navbar/navbar.component.ts
+++ care-plus/src/app/components/navbar/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from "../../logo/logo.component";
+import { Router, RouterModule } from '@angular/router';
 
 
 @Component({
@@ -7,8 +8,16 @@ import { LogoComponent } from "../../logo/logo.component";
     standalone: true,
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.css',
-    imports: [LogoComponent]
+    imports: [LogoComponent,
+        RouterModule
+    ]
 })
 export class NavbarComponent {
+
+    constructor(private router:Router){}
+
+    ir_inicio_sesion(){
+        this.router.navigate(["/inicio_sesion"])
+    }
 
 }
