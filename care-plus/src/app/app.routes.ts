348560a0b193acf538ca10c15861e2d7296df663
ElenaSaugar-eviden <elena.saugar.external@eviden.com>
2024-05-10 09:27:39 +0200

    login comentado

--- care-plus/src/app/app.routes.ts
+++ care-plus/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar/navbar.component';
+import { LoaderComponent } from './components/loader/loader.component';
 
 export const routes: Routes = [
-    {path: '', component: LoginComponent},
-    {path: 'navbar', component: NavbarComponent}
+    // {path: '', component: LoginComponent},
+    {path: 'navbar', component: NavbarComponent},
+    {path: 'loader', component: LoaderComponent}
 ];
