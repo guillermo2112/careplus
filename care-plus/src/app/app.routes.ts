6076dac0fdfcc5c122f96d028a5d94a4eb5f1c31
guillermo2112 <guillermo.moreno.external@eviden.com>
2024-05-10 13:26:19 +0200

    contacto terminado

--- care-plus/src/app/app.routes.ts
+++ care-plus/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { EspecialidadesComponent } from './components/especialidades/especialidades.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 export const routes: Routes = [
     {path: '', redirectTo:'home',pathMatch:'full'},
@@ -12,4 +13,5 @@ export const routes: Routes = [
     {path: 'quienes_somos', component: QuienesSomosComponent},
     {path: 'inicio_sesion', component: LoginComponent},
     {path: 'especialidades', component: EspecialidadesComponent},
+    {path: 'contacto', component: ContactoComponent},
 ];
