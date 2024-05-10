7a6f165604014fd1fe8fd03739867db9b85161be
guillermo2112 <guillermo.moreno.external@eviden.com>
2024-05-10 12:27:36 +0200

    routing v5

--- care-plus/src/app/app.component.ts
+++ care-plus/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { NavbarComponent } from "./components/navbar/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { CarouselComponent } from "./components/carousel/carousel.component";
 import { CardespecialidadesComponent } from './components/cardespecialidades/cardespecialidades.component';
+import { HomeComponent } from "./components/home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent, FooterComponent, CarouselComponent, CardespecialidadesComponent]
+    imports: [RouterOutlet, NavbarComponent, FooterComponent, CarouselComponent, CardespecialidadesComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'care-plus';
--- care-plus/src/app/app.routes.ts
+++ care-plus/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+    {path: '', redirectTo:'home',pathMatch:'full'},
     {path: 'navbar', component: NavbarComponent},
     {path: 'home', component: HomeComponent},
     {path: 'quienes_somos', component: QuienesSomosComponent},
