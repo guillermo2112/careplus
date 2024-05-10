b703de0c8324ac6b26ba6efac2c52a58e9cc1b60
marianazanoletty-eviden <166487190+marianazanoletty-eviden@users.noreply.github.com>
2024-05-10 09:28:37 +0200

    carousel imagenes

--- care-plus/src/app/app.routes.ts
+++ care-plus/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+// import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar/navbar.component';
 
 export const routes: Routes = [
-    {path: '', component: LoginComponent},
+    // {path: '', component: LoginComponent},
     {path: 'navbar', component: NavbarComponent}
 ];
