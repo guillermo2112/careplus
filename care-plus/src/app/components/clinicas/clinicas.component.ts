6745a956d3fe35c3acc5f4e8219e16d271b19fad
guillermo2112 <guillermo.moreno.external@eviden.com>
2024-05-13 16:43:29 +0200

    hospital 3

--- care-plus/src/app/components/clinicas/clinicas.component.ts
+++ care-plus/src/app/components/clinicas/clinicas.component.ts
@@ -1,19 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Hospital } from '../../hospital.model';
 
 @Component({
   selector: 'app-clinicas',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './clinicas.component.html',
   styleUrl: './clinicas.component.css'
 })
 export class ClinicasComponent implements OnInit{
 
+  hospitales:Hospital[];
+
   nombre: string = "hola"
 
   ngOnInit(): void {
-    this.obtener_hospitales();
-    nombre:'hola';
+    this.hospitales.push(
+      new Hospital(1, 100, 'Dirección del Hospital 1', 'Hospital 1', '123-456-7890'),
+      new Hospital(2, 150, 'Dirección del Hospital 2', 'Hospital 2', '123-456-7891'),
+      new Hospital(3, 200, 'Dirección del Hospital 3', 'Hospital 3', '123-456-7892'),
+      new Hospital(4, 250, 'Dirección del Hospital 4', 'Hospital 4', '123-456-7893')
+    );
   }
 
   private obtener_hospitales(){
--- care-plus/src/app/hospital.model.ts
+++ care-plus/src/app/hospital.model.ts
@@ -1,4 +1,17 @@
-export class Hostipal{
-    
+export class Hospital{
+
     id:number;
+    pc:number;
+    address:string;
+    name:string;
+    phone:string;
+
+    constructor(id: number, pc: number, address: string, name: string, phone: string) {
+        this.id = id;
+        this.pc = pc;
+        this.address = address;
+        this.name = name;
+        this.phone = phone;
+      }
+
 }
