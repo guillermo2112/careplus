897cda8852d3fe703aa09b21499ad80c599378a6
guillermo2112 <guillermo.moreno.external@eviden.com>
2024-05-13 17:13:01 +0200

    clinicas terminadas

--- care-plus/src/app/components/clinicas/clinicas.component.ts
+++ care-plus/src/app/components/clinicas/clinicas.component.ts
@@ -13,16 +13,14 @@ import { Hospital } from '../../hospital.model';
 })
 export class ClinicasComponent implements OnInit{
 
-  hospitales:Hospital[];
-
-  nombre: string = "hola"
+  hospitales:Hospital[] = [];
 
   ngOnInit(): void {
     this.hospitales.push(
-      new Hospital(1, 100, 'Dirección del Hospital 1', 'Hospital 1', '123-456-7890'),
-      new Hospital(2, 150, 'Dirección del Hospital 2', 'Hospital 2', '123-456-7891'),
-      new Hospital(3, 200, 'Dirección del Hospital 3', 'Hospital 3', '123-456-7892'),
-      new Hospital(4, 250, 'Dirección del Hospital 4', 'Hospital 4', '123-456-7893')
+      new Hospital(1, 350, 'Calle Principal', 'Hospital Central', '555-123-4567'),
+      new Hospital(2, 200, 'Avenida del Sol', 'Hospital del Este', '555-987-6543'),
+      new Hospital(3, 150, 'Boulevard de la Salud', 'Hospital del Norte', '555-321-6789'),
+      new Hospital(4, 500, 'Calle de la Esperanza', 'Hospital Occidental', '555-789-0123')
     );
   }
 
