fc8327a8130891815ea6774d02fde02559548771
marianazanoletty-eviden <166487190+marianazanoletty-eviden@users.noreply.github.com>
2024-05-14 13:07:18 +0200

    clase y servicio Doctor

---
+++ care-plus/src/app/doctor.ts
@@ -0,0 +1,11 @@
+export class Doctor {
+    id: number;
+    specialtyId: number;
+    userId: number;
+    address: String;
+    birthdate: String;
+    dni: String;
+    licenseNum: number;
+    name: String;
+    phone: String;
+}
