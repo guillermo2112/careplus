938499ed3e2bac418a125bce392b235b8e2af25b
guillermo2112 <guillermo.moreno.external@eviden.com>
2024-05-14 10:27:19 +0200

    clinicas conexcion correcta

--- care-plus/src/app/hospital.model.ts
+++ care-plus/src/app/hospital.model.ts
@@ -15,3 +15,43 @@ export class Hospital{
       }
 
 }
+/*
+export class Hospital {
+  id: number;
+  name: string;
+  address: string;
+  pc: number;
+  phone: string;
+  onDutty: string;
+  province: Province;
+
+  constructor(
+    id: number,
+    name: string,
+    address: string,
+    pc: number,
+    phone: string,
+    onDutty: string,
+    province: Province
+  ) {
+    this.id = id;
+    this.name = name;
+    this.address = address;
+    this.pc = pc;
+    this.phone = phone;
+    this.onDutty = onDutty;
+    this.province = province;
+  }
+}
+
+export class Province {
+  id: number;
+  name: string;
+  onDutty: string;
+
+  constructor(id: number, name: string, onDutty: string) {
+    this.id = id;
+    this.name = name;
+    this.onDutty = onDutty;
+  }
+}*/
