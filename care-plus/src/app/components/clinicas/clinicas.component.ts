d19a121d1d0cad3d4a51f877a23e607dbb0fedfb
guillermo2112 <guillermo.moreno.external@eviden.com>
2024-05-14 08:29:15 +0200

    hospitales 4

--- care-plus/src/app/components/clinicas/clinicas.component.ts
+++ care-plus/src/app/components/clinicas/clinicas.component.ts
@@ -20,7 +20,8 @@ export class ClinicasComponent implements OnInit{
       new Hospital(1, 350, 'Calle Principal', 'Hospital Central', '555-123-4567'),
       new Hospital(2, 200, 'Avenida del Sol', 'Hospital del Este', '555-987-6543'),
       new Hospital(3, 150, 'Boulevard de la Salud', 'Hospital del Norte', '555-321-6789'),
-      new Hospital(4, 500, 'Calle de la Esperanza', 'Hospital Occidental', '555-789-0123')
+      new Hospital(4, 500, 'Calle de la Esperanza', 'Hospital Occidental', '555-789-0123'),
+      new Hospital(5, 18023 , 'Alfonso Comín, 7', 'Hospital Quirónsalud', '932-554-000'),
     );
   }
 
