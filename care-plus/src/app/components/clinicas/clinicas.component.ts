5860cf4b1ff83fb4f30452ccadf24580556b2956
guillermo2112 <guillermo.moreno.external@eviden.com>
2024-05-14 12:08:14 +0200

    clinicas 1

--- care-plus/src/app/components/clinicas/clinicas.component.ts
+++ care-plus/src/app/components/clinicas/clinicas.component.ts
@@ -21,22 +21,13 @@ export class ClinicasComponent implements OnInit{
   hospitales:any[] = [];
 
   ngOnInit(): void {
-    this.obtener_hospitales();
-
-    /*this.hospitales.push(
-      new Hospital(1, 350, 'Calle Principal', 'Hospital Central', '555-123-4567'),
-      new Hospital(2, 200, 'Avenida del Sol', 'Hospital del Este', '555-987-6543'),
-      new Hospital(3, 150, 'Boulevard de la Salud', 'Hospital del Norte', '555-321-6789'),
-      new Hospital(4, 500, 'Calle de la Esperanza', 'Hospital Occidental', '555-789-0123'),
-      new Hospital(5, 18023 , 'Alfonso Comín, 7', 'Hospital Quirónsalud', '932-554-000'),
-    );
-    */
-    
+    this.obtener_hospitales();   
     
   }
 
   private obtener_hospitales(){
     this.hospital_service.obtener_hospitales().subscribe(dato => {
+      console.log(dato);
       this.hospitales = dato;
     });
   }
--- care-plus/src/app/hospital.model.ts
+++ care-plus/src/app/hospital.model.ts
@@ -1,21 +1,3 @@
-/*export class Hospital{
-
-    id:number;
-    pc:number;
-    address:string;
-    name:string;
-    phone:string;
-
-    constructor(id: number, pc: number, address: string, name: string, phone: string) {
-        this.id = id;
-        this.pc = pc;
-        this.address = address;
-        this.name = name;
-        this.phone = phone;
-      }
-
-}*/
-
 export class Hospital {
   id: number;
   name: string;
