b3d2a049decba377f9fae585c3f6401bba949cf9
marianazanoletty-eviden <166487190+marianazanoletty-eviden@users.noreply.github.com>
2024-05-14 10:55:12 +0200

    specialties

--- care-plus/src/app/components/especialidades/especialidades.component.ts
+++ care-plus/src/app/components/especialidades/especialidades.component.ts
@@ -2,12 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { SpecialtyService } from '../../specialty.service';
 import { Specialty } from '../../specialty';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-especialidades',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './especialidades.component.html',
   styleUrl: './especialidades.component.css'
@@ -15,25 +19,25 @@ import { Specialty } from '../../specialty';
 export class EspecialidadesComponent implements OnInit{
 
 
-  specialty:Specialty[];
+  specialty:any[] = [];
   constructor(
     private specialtyService: SpecialtyService,
     private router: Router,
   ) {}
   
   ngOnInit(): void {
-    this.specialty.push(
-      new Specialty(1, 'Calle Principal'),
-      new Specialty(2,'Avenida del Sol'),
-      new Specialty(3,  'Boulevard de la Salud'),
-      new Specialty(4, 'Calle de la Esperanza'),
-      new Specialty(5,  'Alfonso Comín, 7'),
-    );
+    this.listSpecialty();
+      // new Specialty(1, 'Calle Principal'),
+      // new Specialty(2,'Avenida del Sol'),
+      // new Specialty(3,  'Boulevard de la Salud'),
+      // new Specialty(4, 'Calle de la Esperanza'),
+      // new Specialty(5,  'Alfonso Comín, 7'),
+  
   }
   private listSpecialty(){
-    // this.specialtyService.listSpecialty().subscribe(dato =>{
-    //   this.specialty = dato;
-    // });
+    this.specialtyService.listSpecialty().subscribe(dato =>{
+      this.specialty = dato;
+    });
    }
 
 }
