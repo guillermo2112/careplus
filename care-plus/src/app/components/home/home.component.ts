362b98c9ba4238a2815e5404e3e4390b1caf84ac
ElenaSaugar-eviden <elena.saugar.external@eviden.com>
2024-05-16 12:42:13 +0200

    componentes info +doctores en home

--- care-plus/src/app/components/home/home.component.ts
+++ care-plus/src/app/components/home/home.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CarouselComponent } from "./carousel/carousel.component";
 import { CardespecialidadesComponent } from "./cardespecialidades/cardespecialidades.component";
 import { NewsComponent } from "./news/news.component";
+import { DoctoreshomeComponent } from "./doctoreshome/doctoreshome.component";
+import { InfoComponent } from "./info/info.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [CarouselComponent, CardespecialidadesComponent, NewsComponent]
+    imports: [CarouselComponent, CardespecialidadesComponent, NewsComponent, DoctoreshomeComponent, InfoComponent]
 })
 export class HomeComponent {
 
