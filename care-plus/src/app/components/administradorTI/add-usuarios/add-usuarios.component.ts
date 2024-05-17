04f739f8e33c2eba260b926f7e39ddfdafeb166c
guillermo2112 <guillermo.moreno.external@eviden.com>
2024-05-17 12:01:31 +0200

    add_usuarios creado

---
+++ care-plus/src/app/components/administradorTI/add-usuarios/add-usuarios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-usuarios',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-usuarios.component.html',
+  styleUrl: './add-usuarios.component.css'
+})
+export class AddUsuariosComponent {
+
+}
