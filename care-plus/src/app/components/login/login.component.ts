1b414001fb1616b622c2ceda9166e09b12c064c2
CarlosSanchez023 <carlos.sanchez.external@eviden.com>
2024-05-21 12:21:41 +0200

    login con animación

--- care-plus/src/app/components/login/login.component.ts
+++ care-plus/src/app/components/login/login.component.ts
@@ -28,67 +28,62 @@ export class LoginComponent implements OnInit {
 
 
   ngOnInit(): void {
-    //if (typeof document !== 'undefined') {
-    //  let current: anime.AnimeInstance | null = null;
+    if (typeof document !== 'undefined') {
+     let current: anime.AnimeInstance | null = null;
 
-    //  document.querySelector('#email')?.addEventListener('focus', function (e: Event) {
-    //    if (current) current.pause();
-    //    current = anime({
-    //      targets: 'path',
-    //      strokeDashoffset: {
-    //        value: 0,
-    //        duration: 700,
-    //        easing: 'easeOutQuart'
-    //      },
-    //      strokeDasharray: {
-    //        value: '240 1386',
-    //        duration: 700,
-    //        easing: 'easeOutQuart'
-    //      }
-   //     });
-   //   });
+     document.querySelector('#email')?.addEventListener('focus', function (e: Event) {
+       if (current) current.pause();
+       current = anime({
+         targets: 'path',
+         strokeDashoffset: {
+           value: 0,
+           duration: 700,
+           easing: 'easeOutQuart'
+         },
+         strokeDasharray: {
+           value: '240 1386',
+           duration: 700,
+           easing: 'easeOutQuart'
+         }
+       });
+     });
 
-    //  document.querySelector('#password')?.addEventListener('focus', function (e: Event) {
-     //   if (current) current.pause();
-    //   current = anime({
-     //     targets: 'path',
-     //     strokeDashoffset: {
-     //       value: -336,
-      //      duration: 700,
-    //        easing: 'easeOutQuart'
-   //       },
-   //       strokeDasharray: {
-    //        value: '240 1386',
-    //        duration: 700,
-    //        easing: 'easeOutQuart'
-    //      }
-    //    });
-    //  });
+     document.querySelector('#password')?.addEventListener('focus', function (e: Event) {
+       if (current) current.pause();
+      current = anime({
+         targets: 'path',
+         strokeDashoffset: {
+           value: -336,
+           duration: 700,
+           easing: 'easeOutQuart'
+         },
+         strokeDasharray: {
+           value: '240 1386',
+           duration: 700,
+           easing: 'easeOutQuart'
+         }
+       });
+     });
 
-    //  document.querySelector('#submit')?.addEventListener('click', (e: Event) => {
-    //    e.preventDefault(); // Evitar el comportamiento predeterminado del botón
-    //    if (current) current.pause();
-    //    current = anime({
-    //      targets: 'path',
-    //      strokeDashoffset: {
-    //        value: -730,
-     //       duration: 700,
-    //        easing: 'easeOutQuart'
-    //      },
-    //      strokeDasharray: {
-    //        value: '530 1386',
-    //        duration: 700,
-    //        easing: 'easeOutQuart'
-    //      },
-   //     complete: () => {
-    //         //Agregar un retraso de 1 segundo antes de navegar a la página 'navbar'
-     //      setTimeout(() => {
-      //       this.router.navigate(['/home']);
-      //      }, 1000);
-       //   }
-        //});
-      //});
-    //}
+     document.querySelector('#submit')?.addEventListener('click', (e: Event) => {
+       e.preventDefault(); // Evitar el comportamiento predeterminado del botón
+       if (current) current.pause();
+       current = anime({
+         targets: 'path',
+         strokeDashoffset: {
+           value: -730,
+           duration: 700,
+           easing: 'easeOutQuart'
+         },
+         strokeDasharray: {
+           value: '530 1386',
+           duration: 700,
+           easing: 'easeOutQuart'
+         },
+       
+        });
+      });
+    }
     
   }
 
