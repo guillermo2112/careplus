b26c7cc0adb02dbb405ee37f72ce795dc15b2e49
CarlosSanchez023 <carlos.sanchez.external@eviden.com>
2024-05-22 16:15:31 +0200

    recibir correo electónico en contact us

--- care-plus/src/app/components/paciente/contacto/contacto.component.ts
+++ care-plus/src/app/components/paciente/contacto/contacto.component.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+
 
 @Component({
   selector: 'app-contactopaciente',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    FormsModule
   ],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
+  userEmail: string = '';
+  message: string = '';
 
+  constructor() { }
+
+  sendEmail() {
+    const templateParams = {
+      toEmail: this.userEmail,
+      message: this.message,
+    };
+
+    emailjs.send('service_e7aad5d', 'template_e0z73lq', templateParams, 'CnLoD7mBrVO1uaoMr')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+        alert('Email sent successfully!');
+      }, (error) => {
+        console.error(error.text);
+        alert('Failed to send email.');
+      });
+  }
 }
