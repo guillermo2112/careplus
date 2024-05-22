0bb58b92ebe1ed787c4e7d193c5b1262111aea11
marianazanoletty-eviden <166487190+marianazanoletty-eviden@users.noreply.github.com>
2024-05-22 12:55:46 +0200

    clinicas

--- care-plus/src/app/components/paciente/clinicas/clinicas.component.ts
+++ care-plus/src/app/components/paciente/clinicas/clinicas.component.ts
@@ -6,29 +6,31 @@ import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 
 @Component({
-  selector: 'app-clinicaspaciente',
+  selector: 'app-clinicas',
   standalone: true,
   imports: [
     CommonModule,
     FormsModule
   ],
   templateUrl: './clinicas.component.html',
-  styleUrl: './clinicas.component.css'
+  styleUrls: ['./clinicas.component.css']
 })
 export class ClinicasComponent implements OnInit{
-  
+  hospital: any[] = [];
+  hospital_clear: any[] = [];
+  paginatedHospitals: any[] = [];
+  nombre: string = '';
+  provincia: string = '';
+  seleccionados: string[] = [];
+  currentPage: number = 1;
+  itemsPerPage: number = 5;
+  totalPages: number = 0;
+  totalPagesArray: number[] = [];
 
-  constructor(private hospital_service:HospitalService, private router:Router){}
-
-  hospital:any[] = [];
-  hospital_clear:any[] = [];
-  nombre:string = '';
-  provincia:string='';
-  seleccionados:string[]=[];
+  constructor(private hospital_service: HospitalService, private router: Router) {}
 
   ngOnInit(): void {
     this.obtener_hospital();
-        
   }
 
   provincias(event: Event): void {
@@ -37,86 +39,101 @@ export class ClinicasComponent implements OnInit{
     this.buscar_provincia(selectedProvince);
   }
 
-  burcador_nombre(){
-    if(this.nombre.trim() === ''){
+  burcador_nombre() {
+    if (this.nombre.trim() === '') {
       this.limpiar_filtros();
     }
     this.buscar_nombre(this.nombre);
   }
 
-  buscador_provincia(){
-    if(this.nombre.trim() === ''){
+  buscador_provincia() {
+    if (this.nombre.trim() === '') {
       this.limpiar_filtros();
     }
     this.buscar_provincia(this.nombre);
   }
 
-  ordenar_provincia(){ 
+  ordenar_provincia() { 
     this.hospital.sort((a, b) => a.province.id - b.province.id);
+    this.updatePaginatedHospitals();
   }
 
-  ordenar_id(){
+  ordenar_id() {
     this.hospital.sort((a, b) => a.id - b.id);
+    this.updatePaginatedHospitals();
   }
 
-  ordenar_disponibles(){
+  ordenar_disponibles() {
     this.hospital.sort((a, b) => a.onDutty.localeCompare(b.onDutty));
+    this.updatePaginatedHospitals();
   }
 
-  ordenar_nombre(){
+  ordenar_nombre() {
     this.hospital.sort((a, b) => a.name.localeCompare(b.name));
+    this.updatePaginatedHospitals();
   }
 
-  buscar_disponibles(){
+  buscar_disponibles() {
     this.hospital = this.hospital.filter(hospital => hospital.onDutty === 'ACTIVE');
+    this.calculateTotalPages();
+    this.updatePaginatedHospitals();
   }
 
-  buscar_nombre(nombre:string){
-    //this.hospital = this.hospital.filter(hospital => hospital.name.toLowerCase().startsWith(nombre.toLowerCase()));
+  buscar_nombre(nombre: string) {
     const nombres = this.nombre.toLowerCase().split(' ');
     this.hospital = this.hospital.filter(hospital => {
-      return nombres.every(nombre => hospital.name.toLowerCase().includes(nombre))});
+      return nombres.every(nombre => hospital.name.toLowerCase().includes(nombre));
+    });
     if (this.hospital.length === 0) {
       Swal.fire({
         title: "Opps...",
-        text:"No se han encontrado clinicas con el nombre buscado",
+        text: "No se han encontrado clinicas con el nombre buscado",
         icon: "error"
       });
       this.limpiar_filtros();
+    } else {
+      this.calculateTotalPages();
+      this.updatePaginatedHospitals();
     }
   }
 
-  buscar_provincia(provincia:string){
+  buscar_provincia(provincia: string) {
     this.limpiar_filtros();
     this.hospital = this.hospital.filter(hospital => hospital.province.name.toLowerCase().startsWith(provincia.toLowerCase()));
     if (this.hospital.length === 0) {
       Swal.fire({
         title: "Opps...",
-        text:"No se han encontrado clinicas con la provincia buscada",
+        text: "No se han encontrado clinicas con la provincia buscada",
         icon: "error"
       });
       this.limpiar_filtros();
+    } else {
+      this.calculateTotalPages();
+      this.updatePaginatedHospitals();
     }
   }
 
-  limpiar_filtros(){
+  limpiar_filtros() {
     this.hospital = this.hospital_clear.slice();
-    this.nombre='';
+    this.nombre = '';
+    this.calculateTotalPages();
+    this.updatePaginatedHospitals();
   }
 
-  detalles_hospital(id:number){
-    this.router.navigate(['clinica',id]);
+  detalles_hospital(id: number) {
+    this.router.navigate(['clinica', id]);
   }
 
-  private obtener_hospital(){
+  private obtener_hospital() {
     this.hospital_service.obtener_hospitales().subscribe(dato => {
       this.hospital = dato;
       this.hospital_clear = dato;
-      
+      this.calculateTotalPages();
+      this.updatePaginatedHospitals();
     });
   }
 
-  seleccionarAccion(event: Event):void {
+  seleccionarAccion(event: Event): void {
     const selectElement = event.target as HTMLSelectElement;
     const accion = selectElement.options[selectElement.selectedIndex].value;
     this.limpiar_filtros();
@@ -137,28 +154,69 @@ export class ClinicasComponent implements OnInit{
         this.buscar_disponibles();
         break;
       default:
-
         break;
     }
   }
-  value:any;
-  
 
+  updateHospital(id: number): void {
+    this.router.navigate(['update-hospital', id]);
+  }
 
-updateHospital(id: number): void {
-  this.router.navigate(['update-hospital', id]);
-}
+  getHospitalById(id: number): void {
+    this.hospital_service.obtener_hospital_id(id).subscribe(data => {
+      this.hospital = [data];
+    });
+  }
 
+  goToCreate() {
+    window.location.href = '/add-hospital';
+  }
 
+  // Métodos de paginación
+  calculateTotalPages() {
+    this.totalPages = Math.ceil(this.hospital.length / this.itemsPerPage);
+    this.totalPagesArray = Array(this.totalPages).fill(0).map((x, i) => i + 1);
+  }
 
-getHospitalById(id: number): void {
-  this.hospital_service.obtener_hospital_id(id).subscribe(data => {
-    this.hospital = [data];
-  });
-}
+  updatePaginatedHospitals() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedHospitals = this.hospital.slice(startIndex, endIndex);
+  }
 
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePaginatedHospitals();
+    }
+  }
 
-goToCreate() {
-  window.location.href = '/add-hospital';
-}
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updatePaginatedHospitals();
+    }
+  }
+
+  goToPage(page: number) {
+    this.currentPage = page;
+    this.updatePaginatedHospitals();
+  }
+
+  trackById(index: number, hospital: any): number {
+    return hospital.id;
+  }
+
+  getDuttyStatus(status: string): string {
+    switch (status) {
+      case 'ACTIVE':
+        return 'Disponible';
+      case 'INACTIVE':
+        return 'No disponible';
+      case 'SUSPENDED':
+        return 'Suspendido';
+      default:
+        return '';
+    }
+  }
 }
