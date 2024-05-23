152ff659bee5081160561720ff6bf5813fc79ba3
marianazanoletty-eviden <166487190+marianazanoletty-eviden@users.noreply.github.com>
2024-05-23 10:19:48 +0200

    sidebar

--- care-plus/src/app/components/administradorTI/home-admin/home-admin.component.ts
+++ care-plus/src/app/components/administradorTI/home-admin/home-admin.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { SidebarAdminComponent } from "../sidebar-admin/sidebar-admin.component";
 
 @Component({
-  selector: 'app-home-admin',
-  standalone: true,
-  imports: [],
-  templateUrl: './home-admin.component.html',
-  styleUrl: './home-admin.component.css'
+    selector: 'app-home-admin',
+    standalone: true,
+    templateUrl: './home-admin.component.html',
+    styleUrl: './home-admin.component.css',
+    imports: [SidebarAdminComponent]
 })
 export class HomeAdminComponent {
 
