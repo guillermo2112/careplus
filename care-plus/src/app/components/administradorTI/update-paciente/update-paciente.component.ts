6e70387804748ec6311ce5c0d784e616842e44ff
guillermo2112 <guillermo.moreno.external@eviden.com>
2024-05-23 14:34:53 +0200

    actualizar paciente v1

--- care-plus/src/app/components/administradorTI/update-paciente/update-paciente.component.ts
+++ care-plus/src/app/components/administradorTI/update-paciente/update-paciente.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Paciente } from '../../../entities/paciente';
+import { PacienteService } from '../../../services/paciente.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-update-paciente',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './update-paciente.component.html',
   styleUrl: './update-paciente.component.css'
 })
-export class UpdatePacienteComponent {
+export class UpdatePacienteComponent implements OnInit{
 
+  id: number;
+  paciente: Paciente ;
+
+  constructor(
+    private pacienteService: PacienteService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.pacienteService.obtener_pacientes_id(this.id).subscribe(dato => {
+      this.paciente = dato;
+    });
+
+  }
+
+  onSubmit(){
+    this.pacienteService.actualizar_paciente(this.paciente,this.id).subscribe(dato =>{
+      Swal.fire({
+        title: "Success",
+        text: "Paciente actualizado con éxito",
+        icon: "success"
+      }).then(() => {
+          this.router.navigate(['/lista_pacientes']);
+        
+      });
+    });
+  }
 }
--- care-plus/src/app/services/paciente.service.ts
+++ care-plus/src/app/services/paciente.service.ts
@@ -16,8 +16,8 @@ export class PacienteService {
     return this.http.get<Paciente[]>(`${this.baseURL}/all`);
   }
 
-  obtener_pacientes_id(id:number):Observable<Paciente[]>{
-    return this.http.get<Paciente[]>(`${this.baseURL}/${id}`);
+  obtener_pacientes_id(id:number):Observable<Paciente>{
+    return this.http.get<Paciente>(`${this.baseURL}/${id}`);
   }
 
   actualizar_paciente(paciente:Paciente,id:number): Observable<Object>{
