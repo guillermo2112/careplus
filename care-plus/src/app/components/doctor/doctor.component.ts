325d9e560bef44a82b6bdd676d03f847db30b39b
BiancaAlcantaraExternal <167534122+BiancaAlcantaraExternal@users.noreply.github.com>
2024-05-24 08:44:29 +0200

    cambiado el nombre de la entidad Doctor

--- care-plus/src/app/components/doctor/doctor.component.ts
+++ care-plus/src/app/components/doctor/doctor.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { DoctorService } from '../../services/doctor.service';
-import { Doctor } from '../../entities/doctor';
+import { Doctor } from '../../entities/Doctor';
 
 @Component({
   selector: 'app-doctor',
