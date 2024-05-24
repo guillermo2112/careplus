5a3b7d7a2581fac6b49a1e595620fb98d2ff7df3
marianazanoletty-eviden <166487190+marianazanoletty-eviden@users.noreply.github.com>
2024-05-24 09:54:47 +0200

    onDutty

--- care-plus/src/app/entities/usuario.ts
+++ care-plus/src/app/entities/usuario.ts
@@ -1,27 +1,25 @@
-import { OnDutty } from "./OnDutty";
-
 export class Usuario {
   id:number
   username: string;
   password: string;
   role: string;
-  onDutty: OnDutty;
+  onDutty: string;
   enabled: boolean;
 
-  // constructor(
-  //   id:number=0,
-  //   username: string = '',
-  //   password: string = '',
-  //   role: string = '',
-  //   onDutty: OnDutty,
-  //   enabled: boolean = true
-  // ) {
-  //   this.id = id,
-  //   this.username = username;
-  //   this.password = password;
-  //   this.role = role;
-  //   this.onDutty = onDutty;
-  //   this.enabled = enabled;
-  // }
+  constructor(
+    id:number=0,
+    username: string = '',
+    password: string = '',
+    role: string = '',
+    onDutty: string = 'ACTIVE',
+    enabled: boolean = true
+  ) {
+    this.id = id,
+    this.username = username;
+    this.password = password;
+    this.role = role;
+    this.onDutty = onDutty;
+    this.enabled = enabled;
+  }
 }
 
