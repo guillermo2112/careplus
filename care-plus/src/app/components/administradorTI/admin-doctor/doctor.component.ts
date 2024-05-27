5caccf73643c297bbe4e90eb84e859ead14ae32a
Daniel Esteban <daniel.esteban.external@eviden.com>
2024-05-27 12:28:07 +0200

    Paginación doctores

--- care-plus/src/app/components/administradorTI/admin-doctor/doctor.component.ts
+++ care-plus/src/app/components/administradorTI/admin-doctor/doctor.component.ts
@@ -21,6 +21,9 @@ import {AdminSidebarComponent } from "../admin-sidebar/admin-sidebar.component";
 export class AdminDoctorComponent implements OnInit {
 
   doctores: Doctor[] = [];
+  itemsPerPage: number = 9;
+  currentPage: number = 0;
+  totalPages: number = 0;
 
   constructor(
     private doctorService: DoctorService,
@@ -34,8 +37,37 @@ export class AdminDoctorComponent implements OnInit {
   private listDoctor() {
     this.doctorService.listDoctor().subscribe(dato => {
       this.doctores = dato;
- 
+      this.totalPages = Math.ceil(this.doctores.length / this.itemsPerPage);
     });
+  }
+
+  prevPage(event: Event) {
+    event.preventDefault();
+    if (this.currentPage > 0) {
+      this.setPage(event, this.currentPage - 1);
+    }
+  }
+
+  nextPage(event: Event) {
+    event.preventDefault();
+    if (this.currentPage < this.totalPages - 1) {
+      this.setPage(event, this.currentPage + 1);
+    }
+  }
+
+  setPage(event: Event, pageIndex: number) {
+    event.preventDefault();
+    if (pageIndex >= 0 && pageIndex < this.totalPages) {
+      this.currentPage = pageIndex;
+    }
+  }
+
+  chunkArray(myArray: any[], chunk_size: number) {
+    const results = [];
+    for (let i = 0; i < myArray.length; i += chunk_size) {
+      results.push(myArray.slice(i, i + chunk_size));
+    }
+    return results;
   }
 
   updateDoctor(id: number) {
