ae0ed72803ac8363e79e63ad91f2e8db818121f9
marianazanoletty-eviden <166487190+marianazanoletty-eviden@users.noreply.github.com>
2024-05-28 16:10:43 +0200

    header

--- care-plus/src/app/components/paciente/home/home.component.ts
+++ care-plus/src/app/components/paciente/home/home.component.ts
@@ -5,13 +5,14 @@ import { NewsComponent } from "./news/news.component";
 import { DoctoreshomeComponent } from "./doctoreshome/doctoreshome.component";
 import { InfoComponent } from "./info/info.component";
 import { NavbarComponent } from "../navbar/navbar.component";
+import { HeaderComponent } from "../../shared/header/header.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [CarouselComponent, CardespecialidadesComponent, NewsComponent, DoctoreshomeComponent, InfoComponent, HomeComponent, NavbarComponent]
+    imports: [CarouselComponent, CardespecialidadesComponent, NewsComponent, DoctoreshomeComponent, InfoComponent, HomeComponent, NavbarComponent, HeaderComponent]
 })
 export class HomeComponent {
 
