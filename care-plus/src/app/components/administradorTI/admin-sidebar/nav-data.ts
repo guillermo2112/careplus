53a2cfb30292a3044a1f27608e54840dcdb7e891
marianazanoletty-eviden <166487190+marianazanoletty-eviden@users.noreply.github.com>
2024-05-28 09:58:30 +0200

    nav data

--- care-plus/src/app/components/administradorTI/admin-sidebar/nav-data.ts
+++ care-plus/src/app/components/administradorTI/admin-sidebar/nav-data.ts
@@ -1,10 +1,11 @@
 export const navbarData = [
     { routeLink: 'listar-doctor', label: 'Doctores',link:'/admin-doctor'},
+    { routeLink: 'listar-especialidades', label: 'Especialidades',link:'/admin-especialidades'},
+    { routeLink: 'list-clinica',label: 'Clinicas',link:'/clinicas-admin'},
     { routeLink: 'listar-paciente',label: 'Pacientes',link:'/admin-paciente'},
     { routeLink: 'list-calendario',label: 'Calendarios',link:'/admin-calendar'},
-    { routeLink: 'list-clinica',label: 'Clinicas',link:'/clinicas-admin'},
-    { routeLink: 'app-admin-appointmentshift',label: 'Turno Citas',link:'/admin-appointmentshift'},
-    { routeLink: 'listar-especialidades', label: 'Especialidades',link:'/admin-especialidades'},
+    { routeLink: 'app-admin-appointmentshift',label: 'Citas',link:'/admin-appointmentshift'},
+ 
     // turnos, calendarios, 
 
  
