2820bbffe97230e5873eb4dbe312f955fae32d7c
marianazanoletty-eviden <166487190+marianazanoletty-eviden@users.noreply.github.com>
2024-05-28 09:18:56 +0200

    sidebar a los componentes de doctor

--- care-plus/src/app/components/doctor/doctor-calendarios/doctor-calendarios.component.ts
+++ care-plus/src/app/components/doctor/doctor-calendarios/doctor-calendarios.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { DoctorSidebarComponent } from "../doctor-sidebar/doctor-sidebar.component";
 
 @Component({
-  selector: 'app-doctor-calendarios',
-  standalone: true,
-  imports: [],
-  templateUrl: './doctor-calendarios.component.html',
-  styleUrl: './doctor-calendarios.component.css'
+    selector: 'app-doctor-calendarios',
+    standalone: true,
+    templateUrl: './doctor-calendarios.component.html',
+    styleUrl: './doctor-calendarios.component.css',
+    imports: [DoctorSidebarComponent]
 })
 export class DoctorCalendariosComponent {
 
--- care-plus/src/app/components/doctor/doctor-citas/doctor-citas.component.ts
+++ care-plus/src/app/components/doctor/doctor-citas/doctor-citas.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { DoctorSidebarComponent } from "../doctor-sidebar/doctor-sidebar.component";
 
 @Component({
-  selector: 'app-doctor-citas',
-  standalone: true,
-  imports: [],
-  templateUrl: './doctor-citas.component.html',
-  styleUrl: './doctor-citas.component.css'
+    selector: 'app-doctor-citas',
+    standalone: true,
+    templateUrl: './doctor-citas.component.html',
+    styleUrl: './doctor-citas.component.css',
+    imports: [DoctorSidebarComponent]
 })
 export class DoctorCitasComponent {
 
