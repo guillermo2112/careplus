0ba5e8c90b908589c0b70ce9229cd790288efe60
marianazanoletty-eviden <166487190+marianazanoletty-eviden@users.noreply.github.com>
2024-05-28 11:40:37 +0200

    registrado navbar

--- care-plus/src/app/components/paciente/paciente.component.ts
+++ care-plus/src/app/components/paciente/paciente.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LogoComponent } from "../shared/logo/logo.component";
 
 @Component({
-  selector: 'app-paciente',
-  standalone: true,
-  imports: [],
-  templateUrl: './paciente.component.html',
-  styleUrl: './paciente.component.css'
+    selector: 'app-paciente',
+    standalone: true,
+    templateUrl: './paciente.component.html',
+    styleUrl: './paciente.component.css',
+    imports: [LogoComponent]
 })
 export class PacienteComponent {
+  constructor(private router: Router) {}
 
+  ir_inicio_sesion() {
+    this.router.navigate(["/inicio_sesion"]);
+  }
+
+  closeNavbar() {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarCollapse = document.querySelector('#navbarsExample07');
+
+    if (navbarToggler && navbarCollapse) {
+      if (navbarCollapse.classList.contains('show')) {
+        (navbarToggler as HTMLElement).click();
+      }
+    }
+  }
 }
