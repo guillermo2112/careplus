a2cd0a72470c3c901c2e2ecd21bdccf13e4ce67e
marianazanoletty-eviden <166487190+marianazanoletty-eviden@users.noreply.github.com>
2024-05-28 09:16:11 +0200

    doctor data

--- care-plus/src/app/components/doctor/doctor-sidebar/doctor-sidebar-data.ts
+++ care-plus/src/app/components/doctor/doctor-sidebar/doctor-sidebar-data.ts
@@ -2,7 +2,7 @@ export const navbarData = [
     { routeLink: 'doctor-profile',label: 'Perfil',link:'/doctor-profile'},
 
     { routeLink: 'doctor-listar-paciente',label: 'Pacientes',link:'/doctor-lista-pacientes'},
-    { routeLink: 'doctor-calendarios',label: 'Calendarios',link:'/admin-calendar'},
+    { routeLink: 'doctor-calendarios',label: 'Calendarios',link:'/doctor-calendarios'},
     { routeLink: 'doctor-citas',label: 'Citas',link:'/doctor-calendarios'},
     // turnos, calendarios, 
 
