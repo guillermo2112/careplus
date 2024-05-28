f33faee09297e89055f553c5f88b57268554168b
marianazanoletty-eviden <166487190+marianazanoletty-eviden@users.noreply.github.com>
2024-05-28 09:31:37 +0200

    navbar

--- care-plus/src/app/components/paciente/navbar/navbar.component.ts
+++ care-plus/src/app/components/paciente/navbar/navbar.component.ts
@@ -1,24 +1,30 @@
 import { Component } from '@angular/core';
-
 import { Router, RouterModule } from '@angular/router';
 import { LogoComponent } from '../../shared/logo/logo.component';
 
-
 @Component({
-    selector: 'app-navbar',
-    standalone: true,
-    templateUrl: './navbar.component.html',
-    styleUrl: './navbar.component.css',
-    imports: [LogoComponent,
-        RouterModule
-    ]
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  imports: [LogoComponent, RouterModule]
 })
 export class NavbarComponent {
 
-    constructor(private router:Router){}
+  constructor(private router: Router) {}
 
-    ir_inicio_sesion(){
-        this.router.navigate(["/inicio_sesion"])
+  ir_inicio_sesion() {
+    this.router.navigate(["/inicio_sesion"]);
+  }
+
+  closeNavbar() {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarCollapse = document.querySelector('#navbarsExample07');
+
+    if (navbarToggler && navbarCollapse) {
+      if (navbarCollapse.classList.contains('show')) {
+        (navbarToggler as HTMLElement).click();
+      }
     }
-
+  }
 }
