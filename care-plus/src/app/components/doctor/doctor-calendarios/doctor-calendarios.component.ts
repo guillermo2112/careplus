c3c27fb5b1ac45984714a7bf1486c4c8d3261951
marianazanoletty-eviden <166487190+marianazanoletty-eviden@users.noreply.github.com>
2024-05-28 09:14:48 +0200

    componentes de doctor y sus rutas

---
+++ care-plus/src/app/components/doctor/doctor-calendarios/doctor-calendarios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-calendarios',
+  standalone: true,
+  imports: [],
+  templateUrl: './doctor-calendarios.component.html',
+  styleUrl: './doctor-calendarios.component.css'
+})
+export class DoctorCalendariosComponent {
+
+}
---
+++ care-plus/src/app/components/doctor/doctor-citas/doctor-citas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-citas',
+  standalone: true,
+  imports: [],
+  templateUrl: './doctor-citas.component.html',
+  styleUrl: './doctor-citas.component.css'
+})
+export class DoctorCitasComponent {
+
+}
