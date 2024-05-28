59d5d5435fce44e5452d6c29d770bcc4abed6c59
CarlosSanchez023 <carlos.sanchez.external@eviden.com>
2024-05-28 16:15:05 +0200

    image-upload

--- care-plus/src/app/components/doctor/doctor-profile/doctor-profile.component.ts
+++ care-plus/src/app/components/doctor/doctor-profile/doctor-profile.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { DoctorSidebarComponent } from "../doctor-sidebar/doctor-sidebar.component";
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-doctor-profile',
     standalone: true,
     templateUrl: './doctor-profile.component.html',
     styleUrl: './doctor-profile.component.css',
-    imports: [DoctorSidebarComponent]
+    imports: [DoctorSidebarComponent,
+        RouterModule
+    ]
 })
 export class DoctorProfileComponent {
 
+    constructor(private router: Router) { }
+
+
+    editarFoto(){
+        this.router.navigate(['/ima']);
+    }
 }
