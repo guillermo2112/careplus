1803b993c4ee6ec43d6989280624e95eab5e16bd
marianazanoletty-eviden <166487190+marianazanoletty-eviden@users.noreply.github.com>
2024-05-29 14:05:09 +0200

    registrado home

--- care-plus/src/app/components/paciente-registrado/registrado-home/registrado-home.component.ts
+++ care-plus/src/app/components/paciente-registrado/registrado-home/registrado-home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RegistradoSidebarComponent } from "../registrado-sidebar/registrado-sidebar.component";
 import { RegistradoProfileComponent } from "../registrado-profile/registrado-profile.component";
+import { HeaderComponent } from "../../shared/header/header.component";
 
 @Component({
     selector: 'app-registrado-home',
     standalone: true,
     templateUrl: './registrado-home.component.html',
     styleUrl: './registrado-home.component.css',
-    imports: [RegistradoSidebarComponent, RegistradoProfileComponent]
+    imports: [RegistradoSidebarComponent, RegistradoProfileComponent, HeaderComponent]
 })
 export class RegistradoHomeComponent {
 
