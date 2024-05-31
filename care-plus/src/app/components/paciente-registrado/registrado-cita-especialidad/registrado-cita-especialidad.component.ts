c9c71138f93521e678aa1616fd5787c43c5b9291
marianazanoletty-eviden <166487190+marianazanoletty-eviden@users.noreply.github.com>
2024-05-31 13:11:21 +0200

    cita especialidad

--- care-plus/src/app/components/paciente-registrado/registrado-cita-especialidad/registrado-cita-especialidad.component.ts
+++ care-plus/src/app/components/paciente-registrado/registrado-cita-especialidad/registrado-cita-especialidad.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
 import { RegistradoSidebarComponent } from "../registrado-sidebar/registrado-sidebar.component";
+import { SpecialtyService } from '../../../services/specialty.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { Specialty } from '../../../entities/specialty';
 
 @Component({
     selector: 'app-registrado-cita-especialidad',
@@ -9,6 +13,58 @@ import { RegistradoSidebarComponent } from "../registrado-sidebar/registrado-sid
     styleUrl: './registrado-cita-especialidad.component.css',
     imports: [HeaderComponent, RegistradoSidebarComponent]
 })
-export class RegistradoCitaEspecialidadComponent {
+export class RegistradoCitaEspecialidadComponent implements OnInit{
 
-}
+    constructor(
+        private specialtyService:SpecialtyService,
+        private router:Router
+      ){}
+
+
+ specialty:Specialty[] = [];
+ id:number;
+ 
+    ngOnInit(): void {
+        this.getHospitalById(this.id);
+    }
+
+
+   
+
+    // listSpecialty(){
+    //     this.specialtyService.getSpecialty().subscribe(dato => {
+    //       this.specialty= dato;
+    //     });
+    //   }
+
+      getHospitalById(id: number): void {
+        this.specialtyService.getSpecialtyById(id).subscribe(data => {
+          this.specialty = [data];
+        });
+      }
+      
+
+
+    specialtiesAll(event: Event): void {
+        const selectElement = event.target as HTMLSelectElement;
+        const selectedProvince = selectElement.options[selectElement.selectedIndex].text;
+        this.buscar_provincia(selectedProvince);
+      }
+      buscar_provincia(specialty:string){
+        this.limpiar_filtros();
+        // this.specialty = this.specialty.filter(specialty => specialty.name.toLowerCase().startsWith(specialty.toLowerCase()));
+        if (this.specialty.length === 0) {
+          Swal.fire({
+            title: "Opps...",
+            text:"No se han encontrado clinicas con la provincia buscada",
+            icon: "error"
+          });
+          this.limpiar_filtros();
+        }
+      }
+    
+      limpiar_filtros(){
+        // this.specialty = this.hospital_clear.slice();
+        // this.nombre='';
+      }
+    }
