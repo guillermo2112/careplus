73e974ae19dcd9b023733ec7e4c00e0dacfebdd9
Daniel Esteban <daniel.esteban.external@eviden.com>
2024-05-31 13:50:22 +0200

    Paginación doctor-paciente

--- care-plus/src/app/components/doctor/doctor-lista-pacientes/doctor-lista-pacientes.component.ts
+++ care-plus/src/app/components/doctor/doctor-lista-pacientes/doctor-lista-pacientes.component.ts
@@ -11,66 +11,68 @@ import { ClinicasProfileService } from '../../../services/clinicas-profile.servi
 import Swal from 'sweetalert2';
 
 @Component({
-    selector: 'app-doctor-lista-pacientes',
-    standalone: true,
-    templateUrl: './doctor-lista-pacientes.component.html',
-    styleUrl: './doctor-lista-pacientes.component.css',
-    imports: [
-        FormsModule,
-        CommonModule,
-        DoctorSidebarComponent,
-        HeaderComponent
-    ]
+  selector: 'app-doctor-lista-pacientes',
+  standalone: true,
+  templateUrl: './doctor-lista-pacientes.component.html',
+  styleUrls: ['./doctor-lista-pacientes.component.css'],
+  imports: [
+    FormsModule,
+    CommonModule,
+    DoctorSidebarComponent,
+    HeaderComponent
+  ]
 })
-export class DoctorListaPacientesComponent implements OnInit{
+export class DoctorListaPacientesComponent implements OnInit {
 
-  pacientes:Paciente [] = [];
-  pacientes_clear:Paciente [] = [];
-  perfil:ClinicaProfile = new ClinicaProfile();
-  name:string;
-  ide:string;
+  pacientes: Paciente[] = [];
+  pacientes_clear: Paciente[] = [];
+  perfil: ClinicaProfile = new ClinicaProfile();
+  name: string;
+  ide: string;
 
-  constructor(private paciente_service:PacienteService, private router:Router,private perfilService:ClinicasProfileService){}
+  tamañoPag: number = 8;
+  paginaActual: number = 1;
+  totalPag: number = 1;
+
+  constructor(private paciente_service: PacienteService, private router: Router, private perfilService: ClinicasProfileService) { }
 
   ngOnInit(): void {
     this.obtener_pacientes();
   }
 
-  obtener_pacientes(){
-    this.paciente_service.getPatient().subscribe(dato =>{
-      this.pacientes=dato;
-      this.pacientes_clear=dato;
-    })
+  obtener_pacientes() {
+    this.paciente_service.getPatient().subscribe(dato => {
+      this.pacientes = dato;
+      this.pacientes_clear = dato;
+      this.totalPag = Math.ceil(this.pacientes.length / this.tamañoPag);
+      this.paginatePacientes();
+    });
   }
 
-  async existe_clinical_profile(id:number){
-
+  async existe_clinical_profile(id: number) {
     let existProfile: Boolean = await this.perfilService.comprobarPelfilClinico(id);
     return existProfile;
   }
 
-  goToClinicalProfile(){
-    this.router.navigate(['doctor-clinical-profile',this.perfil.id]);
+  goToClinicalProfile() {
+    this.router.navigate(['doctor-clinical-profile', this.perfil.id]);
   }
 
-  ponerPerfil(id:number){
-    this.paciente_service.getPatientById(id).subscribe(dato =>{
+  ponerPerfil(id: number) {
+    this.paciente_service.getPatientById(id).subscribe(dato => {
       this.perfil.patient = dato;
       this.crearPerfilClinico();
     });
   }
 
-  crearPerfilClinico(){
-    
+  crearPerfilClinico() {
     this.perfil.date = this.getFormattedDate();
-    this.perfil.allergy='';
-    this.perfil.report='';
-    console.log(this.perfil);
-    this.perfilService.createClinicasProfile(this.perfil).subscribe(dato =>{
+    this.perfil.allergy = '';
+    this.perfil.report = '';
+    this.perfilService.createClinicasProfile(this.perfil).subscribe(dato => {
       console.log(this.perfil);
       alert();
     });
-
   }
 
   getFormattedDate(): string {
@@ -102,7 +104,10 @@ export class DoctorListaPacientesComponent implements OnInit{
   limpiar_filtros(): void {
     this.pacientes = [...this.pacientes_clear];
     this.name = '';
-    this.ide='';
+    this.ide = '';
+    this.paginaActual = 1;
+    this.totalPag = Math.ceil(this.pacientes.length / this.tamañoPag);
+    this.paginatePacientes();
   }
 
   burcador_id(): void {
@@ -127,6 +132,9 @@ export class DoctorListaPacientesComponent implements OnInit{
       });
       this.limpiar_filtros();
     }
+    this.totalPag = Math.ceil(this.pacientes.length / this.tamañoPag);
+    this.paginaActual = 1;
+    this.paginatePacientes();
   }
 
   burcador_nombre(): void {
@@ -142,6 +150,75 @@ export class DoctorListaPacientesComponent implements OnInit{
       });
       this.limpiar_filtros();
     }
+    this.totalPag = Math.ceil(this.pacientes.length / this.tamañoPag);
+    this.paginaActual = 1;
+    this.paginatePacientes();
   }
 
+  paginatePacientes(): void {
+    const startIndex = (this.paginaActual - 1) * this.tamañoPag;
+    const endIndex = startIndex + this.tamañoPag;
+    this.pacientes = this.pacientes_clear.slice(startIndex, endIndex);
+  }
+
+  changePage(page: number): void {
+    if (page > 0 && page <= this.totalPag) {
+      this.paginaActual = page;
+      this.paginatePacientes();
+    }
+  }
+
+  nextPage(): void {
+    if (this.paginaActual < this.totalPag) {
+      this.paginaActual++;
+      this.paginatePacientes();
+    }
+  }
+
+  previousPage(): void {
+    if (this.paginaActual > 1) {
+      this.paginaActual--;
+      this.paginatePacientes();
+    }
+  }
+
+  setPage(page: number | string): void {
+    if (page !== '...' && +page > 0 && +page <= this.totalPag) {
+      this.paginaActual = +page;
+      this.paginatePacientes();
+    }
+  }
+
+  getPages(): (number | string)[] {
+    const totalVisiblePages = 5;
+    const pages: (number | string)[] = [];
+    if (this.totalPag <= totalVisiblePages + 1) {
+      for (let i = 1; i <= this.totalPag; i++) {
+        pages.push(i);
+      }
+    } else {
+      if (this.paginaActual <= 3) {
+        for (let i = 1; i <= totalVisiblePages; i++) {
+          pages.push(i);
+        }
+        pages.push('...');
+        pages.push(this.totalPag);
+      } else if (this.paginaActual >= this.totalPag - 2) {
+        pages.push(1);
+        pages.push('...');
+        for (let i = this.totalPag - totalVisiblePages + 1; i <= this.totalPag; i++) {
+          pages.push(i);
+        }
+      } else {
+        pages.push(1);
+        pages.push('...');
+        for (let i = this.paginaActual - 1; i <= this.paginaActual + 1; i++) {
+          pages.push(i);
+        }
+        pages.push('...');
+        pages.push(this.totalPag);
+      }
+    }
+    return pages;
+  }
 }
